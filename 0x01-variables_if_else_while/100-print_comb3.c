4398303898f258c0e3bfedf0c4fe2ab5e94d9ee3
DagiiM <douglasmutethia2017@gmail.com>
2022-03-11 22:21:22 +0300

    for loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,36 +6,32 @@
  */
 int main(void)
 {
-    int n, k = 0, m;
-
-    for (n = 0; n < 9; n++)
-    {
-        for (m = k ; m <= 9; m++)
-        {
-            if (n != m)
-            {
-                putchar(n);
-                putchar(m);
-            }
-
-            if (n == m)
-            {
-                continue;
-            }
-
-            if (n == '8' && m == '9')
-            {
-                break;
-            }
-            else
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        k++;
-    }
-    putchar('\n');
-    
+int n, k = 0, m;
+for (n = 0; n < 9; n++)
+{
+for (m = k ; m <= 9; m++)
+{
+if (n != m)
+{
+putchar(n);
+putchar(m);
+}
+if (n == m)
+{
+continue;
+}
+if (n == '8' && m == '9')
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+k++;
+}
+putchar('\n');
 return (0);
 }
